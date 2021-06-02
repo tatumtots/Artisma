6990983869ac7d15f2accd695de9505eeefe7f7e
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-06-02 17:28:48 -0600

    Home Page Carousel

--- Components/Home.js
+++ Components/Home.js
@@ -1,5 +1,15 @@
-import React from "react"
-import { StyleSheet, TextInput, View, Text, Button, Image, TouchableOpacity } from "react-native"
+import React, { useRef } from "react"
+import { 
+  StyleSheet, 
+  View, 
+  Text, 
+  TouchableOpacity, 
+  ScrollView, 
+  SafeAreaView, 
+  ImageBackground, 
+  Dimensions, 
+  Animated 
+} from "react-native"
 
 import { connect } from "react-redux"
 
@@ -8,88 +18,107 @@ function mapStateToProps(state) {
   return { state }
 }
 
-const Home = (props) => {
-  // console.log("this is PROPS:", props)
-  const {navigation} = props
-  return (
-    <View style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          // width: "80%",
-          height: 600,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "purple",
-          borderColor: "black",
-          borderWidth: 1,
-        }}
+const Offset = 40
+const ItemWidth = Dimensions.get("window").width - (Offset * 2)
+const ItemHeight = 250
+
+const cards = [
+  {id: 1, title: "Explore Digital Paintings", posterUrl: require("../assets/images/galaxyraccoon.jpg")},
+  {id: 2, title: "Find Art That Speaks", posterUrl: require("../assets/images/blue_volcano.jpg")},
+  {id: 3, title: "Sell Your Pieces", posterUrl: require("../assets/images/burnt-trash.jpg")},
+]
+
+function Home() {
+  const scrollX = useRef(new Animated.Value(0)).current
+
+  return(
+    <SafeAreaView style={styles.bannerContainer}>
+      <ScrollView
+      horizontal={true}
+      decelerationRate={"normal"}
+      snapToInterval={ItemWidth}
+      bounces={false}
+      style={{ marginTop:0, paddingHorizontal: 0 }}
+      showsHorizontalScrollIndicator={false}
+      disableIntervalMomentum
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { x: scrollX }}}],
+        {useNativeDriver: false}
+      )}
+      scrollEventThrottle={20}
       >
-      <TouchableOpacity onPress={() => navigation.navigate("Painting")}>
-        <Image
-          style={{ width: 120, height: 120, borderRadius: 400 / 2 }}
-          source={props.state.image.map((i) => i.image)[2]}
-        />
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          backgroundColor: "red",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          borderColor: "black",
-          borderWidth: 1,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          borderColor: "black",
-          borderWidth: 1,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "green",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          borderColor: "black",
-          borderWidth: 1,
-        }}
-      ></View>
-      
-    </View>
+        {cards.map((item, idx) => {
+          const inputRange = [
+            (idx - 1) * ItemWidth,
+            idx * ItemWidth,
+            (idx + 1) * ItemWidth,
+          ]
+
+          const translate = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.85, 1, 0.85],
+          })
+
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.5, 1, 0.5],
+          })
+
+          return(
+            <Animated.View
+              key={item.id}
+              style={{
+                width: ItemWidth,
+                height: ItemHeight,
+                marginLeft: idx === 0 ? Offset : undefined,
+                marginRight: idx === cards.length -1 ? Offset: undefined,
+                opacity: opacity,
+                transform: [{ scale: translate}]
+              }}
+            >
+              <ImageBackground 
+                source={item.posterUrl}
+                style={styles.bannerCard}
+              />
+              <View style={styles.bannerTextContainer}>
+                <Text style={styles.bannerText }>{item.title}</Text>
+              </View>
+              
+            </Animated.View>
+          )
+        })}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bannerContainer: {
     flex: 1,
+    backgroundColor: "#001219"
   },
-  textContainer: {
+  bannerCard: {
+    flex: 1,
+    resizeMode: "cover",
     justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
   },
-  inlineView: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+  bannerTextContainer: {
+    position: 'absolute', 
+    top: 10, 
+    left: 0, 
+    right: 0, 
+    bottom: 0, 
+    alignItems: 'center'
   },
-  addInput: {
-    borderBottomColor: "black",
-    borderBottomWidth: 3,
-    padding: 10,
-    width: "80%",
-  },
+  bannerText: {
+    fontFamily: "Cairo-Regular",
+    color: "#EBEBEB",
+    fontSize: 26,
+    backgroundColor: 'rgba(0, 18, 25, 0.60)',
+    paddingLeft: 10,
+    paddingRight: 10,
+  }
 })
-
 // <Text
 //   style={{
 //     fontFamily: "Cairo-Regular",
