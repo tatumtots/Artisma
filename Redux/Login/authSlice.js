78eccf0d7e887cfcc9a6672a90692475dab415e7
cjmcclure1337 <62575120+cjmcclure1337@users.noreply.github.com>
2021-08-07 17:35:18 -0500

    Added HTTPS POST, not working yet

--- Redux/Login/authSlice.js
+++ Redux/Login/authSlice.js
@@ -9,6 +9,29 @@ const slice = createSlice({
     reducers:{
         signIn:(state, action) => {
             const {email, password} = action.payload;
+
+            console.log("Sending Request")
+            fetch("https://localhost:3443/users/login", {
+                method: "POST",
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: email,
+                    password: password
+                })
+            })
+            .then((response) => {
+                console.log("Response Received");
+                response.json()
+            })
+            .then((json) => {
+                console.log("Token:", json.token);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
             state.LoggedIn = true
             state.admin = true
         },
